532b56c636733643865a1a2404ad7c639c8daf8f
VitorBaldasso <vitor.baldasso05@gmail.com>
2025-10-16 13:17:15 -0300

    Inicializando projeto web API Csharp

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,26 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> Filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        Filmes.Add(filme);
+        Console.WriteLine($"{filme.Titulo} : {filme.Duracao}");
+    }
+
+    [HttpGet]
+    public List<Filme> PegarFilmes()
+    {
+        return Filmes;
+    }
+
+}
---
+++ Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O título do filme é obrigatório!")] 
+        public string Titulo { get; set; }
+        
+        [Required]
+        [Range(70,600, ErrorMessage = "A duração do filme no minimo tem que ter 70 e no máximo 600!")]
+        public int Duracao { get; set; }
+
+        [Required(ErrorMessage = "O título do Genero é obrigatório!")] 
+        [MaxLength(50, ErrorMessage = "O tamanho do genero não pode exeder 50 caracteres")]
+        public string Genero { get; set; }
+
+    }
+}
