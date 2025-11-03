82c36265efb1b91514158fb292d4fe18648f4002
VitorBaldasso <vitor.baldasso05@gmail.com>
2025-11-03 11:58:04 -0300

    Alterações para boas praticas do mercado

---
+++ Data/Configuration/FilmesTypeConfiguration.cs
@@ -0,0 +1,6 @@
+﻿namespace FilmesAPI.Data.Configuration
+{
+    public class FilmesTypeConfiguration
+    {
+    }
+}
---
+++ Data/Configuration/SessaoTypeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FilmesAPI.Data.Configuration
+{
+    public class SessaoTypeConfiguration : IEntityTypeConfiguration<Sessao>
+    {
+        public void Configure(EntityTypeBuilder<Sessao> builder)
+        {
+            builder.ToTable("Sessoes");
+
+            builder.HasKey(x => x.Id);
+
+
+        }
+    }
+}
